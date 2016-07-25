b90a295d0556dc9f1cdb8ef112cfafbb6fb187d9
oioppp <life60323@gmail.com>
2016-07-25 21:45:14 +0800

    modify Compare

--- Q412.c
+++ Q412.c
@@ -6,24 +6,17 @@ command line add -lm in gcc
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-int compare(int a,int b,int r){
-	int c;
-	if(r==1 || a==1 ||b==1) return 1;
-	else if(r==0) return 0;
-	else{
-		if(a>b) c = compare(a%b,b,a%b);
-		else c = compare(a,b%a,b%a);
-		return c;
-	}
+int compare(int a,int b){
+	while((a %= b) && (b %= a));
+	if((a+b)==1) return 1;
+	else return 0;
 }
 int main(int argc, char const *argv[])
 {
 	int count, input;
-	while(1){
+	while(scanf("%d",&count) && count!=0){
 		double pi;
 		int i, j, e = 0, o = 0, sum = 0;
-		scanf("%d",&count);
-		if(count == 0) break;
 		int even[count];
 		int odd[count];
 		for(i=0; i<count; i++){
@@ -38,19 +31,18 @@ int main(int argc, char const *argv[])
 		}
 		for(i=0;i<o;i++){
 			int k;
-			for(j=0;j<e;j++){
-				sum += compare(odd[i], even[j], 2);
-			}
-			for(j=i+1;j<o;j++){
-				sum += compare(odd[i], odd[j], 2);
-			}
+			for(j=0;j<e;j++)
+				sum += compare(odd[i], even[j]);
+			for(j=i+1;j<o;j++)
+				sum += compare(odd[i], odd[j]);
 		}
 		if(sum>0){
 			int num = (count*(count-1))/2;
 			pi = sqrt((double)6*num/sum);
 			printf("%lf\n",pi);
 		}
-		else printf("No estimate for this data set.\n");
+		else 
+			printf("No estimate for this data set.\n");
 	}
 	return 0;
 }
