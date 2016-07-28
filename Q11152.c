2a79bebfcbed69ea0f4ac4e3ab1eb6b1e1556f77
oioppp <life60323@gmail.com>
2016-07-28 17:54:18 +0800

    ~

--- Q11152.c
+++ Q11152.c
@@ -8,7 +8,7 @@ int main(int argc, char const *argv[])
 	double sunflowers, violates, roses, s;
 	while(scanf("%lf %lf %lf", &a, &b, &c)){
 		s = (a + b + c) / 2;
-		violates = sqrt(s*(s-a)*(s-b)*(s-c));
+		violates = sqrt(s * (s - a) * (s - b) * (s - c));
 		sunflowers = pow((a * b * c) / (4 * violates), 2) * PI;
 		roses = pow((2 * violates) / (a + b + c), 2) * PI;
 		printf("%.4lf %.4lf %.4lf\n", sunflowers - violates, violates - roses, roses);
